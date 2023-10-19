d1abf933a13b0f42bd07949dc97ed3acceb114da
Alex <65155828+CSWangXY@users.noreply.github.com>
2023-10-19 07:38:59 +0000

    10191

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
@@ -18,12 +17,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn is_true_when_even() {
-        assert!();
+    fn is_true_when_even() 
+    {
+        
+        assert!(is_even(100));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(is_even(100));
     }
 }
